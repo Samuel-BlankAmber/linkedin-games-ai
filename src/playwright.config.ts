393eec1116ebe3ffe5b78ed688fa3d429805d069
BlankAmber <145449058+BlankAmber@users.noreply.github.com>
2025-03-28 09:49:20 +0000

    Make Playwright window smaller

--- src/playwright.config.ts
+++ src/playwright.config.ts
@@ -3,4 +3,10 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testMatch: '**/*.spec.ts',
   timeout: 60 * 1000,
+  use: {
+    launchOptions: {
+      args: ['--window-size=426,600', '--window-position=0,0']
+    },
+    viewport: null,
+  },
 });
